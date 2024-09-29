6eecbfec2591e61b9f085f1fbde984664c446e28
JSurface <johnmedgar.2nd@gmail.com>
2024-09-29 06:19:26 +0100

    Thread Wizard Birth

---
+++ SunsetCore/ThreadWizard.hpp
@@ -0,0 +1,25 @@
+/*
+    This file will be responsible for containing
+    the mythical "Thread Wizard" who will control
+    and manage the different threads the engine
+    allowing for faster physics handling and
+    processing so that applications can do
+    MORE in LESS time.
+
+    This will be worked on seperate to the main
+    engine and will probably merge at a MUCH
+    later date.
+*/
+#pragma once
+
+namespace SunsetEngine
+{
+    class ThreadWizard
+    {
+        private:
+        protected:
+        public:
+        ThreadWizard(){}
+        ~ThreadWizard(){}
+    };
+};
